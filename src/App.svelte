e588ba4dec8694a60b1d9c89e4d966c0ffe8a055
Frozen FIsh <976499226@qq.com>
2022-08-31 10:24:51 +0800

    chore: test

--- src/App.svelte
+++ src/App.svelte
@@ -18,7 +18,7 @@
     <Counter />
   </div>
 
-  <p>
+  <p class="bg-red-500">
     Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
   </p>
 
